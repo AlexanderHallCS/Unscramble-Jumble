8b06f2e966fffcfafd420078e023e3e702041feb
AlexanderHallCS <hallale21@yahoo.com>
2020-03-19 22:47:39 -0500

    Add Google AdMob framework.

--- Unscramble It!/ViewController.swift
+++ Unscramble It!/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, GADBannerViewDelegate {
+    
+    @IBOutlet var bannerView: GADBannerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        bannerView.delegate = self
+    }
+    
+    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
+        print("Ad was received!")
+    }
+    
+    func adView(_ bannerView: GADBannerView, didFailToReceiveAdWithError error: GADRequestError) {
+        print(error)
     }
 
 
