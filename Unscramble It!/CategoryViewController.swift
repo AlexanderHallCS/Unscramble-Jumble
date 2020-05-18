c3ff94d73bf98cc610bbf7a3937abd439660c823
AlexanderHallCS <hallale21@yahoo.com>
2020-05-17 23:43:26 -0500

    Fix CategoryViewController not deallocating by adding five segues from it to GameViewController and removing it and its view every time it segues.

--- Unscramble It!/CategoryViewController.swift
+++ Unscramble It!/CategoryViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class CategoryViewController: UIViewController {
     
+    enum Segues {
+        static let adjectivesToGame = "adjectivesToGame"
+        static let commonWordsToGame = "commonWordsToGame"
+        static let countriesToGame = "countriesToGame"
+        static let natureToGame = "natureToGame"
+        static let spaceToGame = "spaceToGame"
+    }
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -20,6 +28,24 @@ class CategoryViewController: UIViewController {
         
        // self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
+    
+    // segue to GameViewController and remove CategoryViewController right after
+    @IBAction func adjectivesToGame(_ sender: UIButton) {
+        segueAndRemoveSelf(segueName: Segues.adjectivesToGame)
+    }
+    @IBAction func commonWordsToGame(_ sender: UIButton) {
+        segueAndRemoveSelf(segueName: Segues.commonWordsToGame)
+    }
+    @IBAction func countriesToGame(_ sender: UIButton) {
+        segueAndRemoveSelf(segueName: Segues.countriesToGame)
+    }
+    @IBAction func natureToGame(_ sender: UIButton) {
+        segueAndRemoveSelf(segueName: Segues.natureToGame)
+    }
+    @IBAction func spaceToGame(_ sender: UIButton) {
+        segueAndRemoveSelf(segueName: Segues.spaceToGame)
+    }
+    
     
     deinit {
         print("did deinit2!")
@@ -47,17 +73,45 @@ class CategoryViewController: UIViewController {
             self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
         });
     }
-
     
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        print("was called!")
-        self.presentingViewController?.dismiss(animated: true, completion: nil)
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let identifier = segue.identifier {
+            switch identifier {
+                case Segues.adjectivesToGame:
+                    if let destVC = segue.destination as? GameViewController {
+                        // pass data that makes it an adjectives VC
+                    }
+                case Segues.commonWordsToGame:
+                    if let destVC = segue.destination as? GameViewController {
+                        // pass data that makes it a common words VC
+                    }
+                case Segues.countriesToGame:
+                    if let destVC = segue.destination as? GameViewController {
+                        // pass data that makes it a countries VC
+                    }
+                case Segues.natureToGame:
+                    if let destVC = segue.destination as? GameViewController {
+                        // pass data that makes it a nature VC
+                    }
+                case Segues.spaceToGame:
+                    if let destVC = segue.destination as? GameViewController {
+                        // pass data that makes it a space VC
+                    }
+                default: break
+            }
+        }
     }
     
+}
 
+extension CategoryViewController {
+    func segueAndRemoveSelf(segueName: String) {
+        self.performSegue(withIdentifier: segueName, sender: self)
+        self.view.removeFromSuperview()
+        self.removeFromParent()
+        self.willMove(toParent: nil)
+    }
 }
