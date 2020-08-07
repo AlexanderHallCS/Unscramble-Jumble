5330b96c81254b1fee7273c1f214a7ce77f894f7
AlexanderHallCS <hallale21@yahoo.com>
2020-08-07 11:37:32 -0500

    Add yellow border on letters and saving when chosen. Make letters in shop view square.

--- Unscramble Jumble!/ShopViewController.swift
+++ Unscramble Jumble!/ShopViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ShopViewController: UIViewController {
 
+    
+    @IBOutlet var defaultLetter: UIImageView!
+    @IBOutlet var woodenLetter: UIImageView!
+    @IBOutlet var metalLetter: UIImageView!
+    
+    @IBOutlet var purchaseAllLabel: UILabel!
+    
+    var coreDataManager = CoreDataManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //coreDataManager.addAndSaveIAPData(currentLetterSkin: coreDataManager.fetchIAPData().currentLetterSkin,hasPurchased: true)
+        colorAndPrepareLetters()
         self.view.backgroundColor = UIColor.black.withAlphaComponent(0.6)
         self.animateIn()
+    }
+    
+    // makes it so that the grayed out wooden and metal image views are not gray if the user purchased the bundle
+    // also enables user interaction
+    // also adds yellow border to the chosen letter
+    private func colorAndPrepareLetters() {
+        //print("VALUE: \(coreDataManager.fetchIAPData().hasPurchased)")
+        if coreDataManager.fetchIAPData().hasPurchased == true {
+            //print("ANY TRUERS?")
+            woodenLetter.image = UIImage(named: "A Wooden")
+            metalLetter.image = UIImage(named: "A Metal")
+            defaultLetter.isUserInteractionEnabled = true
+            woodenLetter.isUserInteractionEnabled = true
+            metalLetter.isUserInteractionEnabled = true
+        }
+        switch getLetterImageViewFromCurrentSkin() {
+            case defaultLetter:
+                defaultLetter.layer.borderColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+                defaultLetter.layer.borderWidth = defaultLetter.frame.height/17
+            case woodenLetter:
+                woodenLetter.layer.borderColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+                woodenLetter.layer.borderWidth = woodenLetter.frame.height/17
+            case metalLetter:
+                metalLetter.layer.borderColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+                metalLetter.layer.borderWidth = metalLetter.frame.height/17
+            default:
+                break
+        }
     }
     
     @IBAction func backOut(_ sender: UIButton) {
@@ -38,4 +77,56 @@ class ShopViewController: UIViewController {
         });
     }
 
+    private func getLetterImageViewFromCurrentSkin() -> UIImageView {
+        
+        var letterImageView = UIImageView()
+        
+        switch coreDataManager.fetchIAPData().currentLetterSkin {
+        case 0:
+            letterImageView = defaultLetter
+        case 1:
+            letterImageView = woodenLetter
+        case 2:
+            letterImageView = metalLetter
+        default:
+            break
+        }
+        
+        return letterImageView
+    }
+    
+    @IBAction func promptPurchase(_ sender: UIButton) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let firstTouch = touches.first {
+            //print("Touch Check Value: \(coreDataManager.fetchIAPData().hasPurchased)")
+            if coreDataManager.fetchIAPData().hasPurchased == true {
+                switch firstTouch.view {
+                case defaultLetter:
+                    defaultLetter.layer.borderColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+                    defaultLetter.layer.borderWidth = defaultLetter.frame.width/17
+                    getLetterImageViewFromCurrentSkin().layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+                    coreDataManager.addAndSaveIAPData(currentLetterSkin: 0, hasPurchased: true)
+                    //print("Touch Check Value Default Letter: \(coreDataManager.fetchIAPData().hasPurchased)")
+                case woodenLetter:
+                    woodenLetter.layer.borderColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+                    woodenLetter.layer.borderWidth = woodenLetter.frame.width/17
+                    getLetterImageViewFromCurrentSkin().layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+                    coreDataManager.addAndSaveIAPData(currentLetterSkin: 1, hasPurchased: true)
+                    //print("Touch Check Value Wooden: \(coreDataManager.fetchIAPData().hasPurchased)")
+                case metalLetter:
+                    metalLetter.layer.borderColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+                    metalLetter.layer.borderWidth = metalLetter.frame.width/17
+                    getLetterImageViewFromCurrentSkin().layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+                    coreDataManager.addAndSaveIAPData(currentLetterSkin: 2, hasPurchased: true)
+                    //print("Touch Check Value Metal: \(coreDataManager.fetchIAPData().hasPurchased)")
+                default:
+                    break
+                }
+            }
+        }
+    }
+    
 }
